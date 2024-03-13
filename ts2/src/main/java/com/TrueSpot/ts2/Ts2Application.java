98516feccccf4adf4b9659265b063bdd66d9ce6b
jsw2840 <jis2843@naver.com>
2024-03-13 15:41:24 +0900

    프로젝트 생성

---
+++ ts2/src/main/java/com/TrueSpot/ts2/Ts2Application.java
@@ -0,0 +1,13 @@
+package com.TrueSpot.ts2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ts2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ts2Application.class, args);
+	}
+
+}
---
+++ ts2/src/test/java/com/TrueSpot/ts2/Ts2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrueSpot.ts2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ts2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
